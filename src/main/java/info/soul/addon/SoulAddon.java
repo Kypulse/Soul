f95f10ec4402b5c37c8e51ea8239e6bbb1a42dfd
Kypulse <munro2767@gmail.com>
2025-08-02 19:05:58 +0100

    Update SoulAddon.java

--- src/main/java/info/soul/addon/SoulAddon.java
+++ src/main/java/info/soul/addon/SoulAddon.java
@@ -4,6 +4,7 @@ import info.soul.addon.modules.*;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
@@ -14,19 +15,24 @@ public class SoulAddon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
-    Modules.get().add(new BlockESP());;
-    Modules.get().add(new TunnelBaseFinder());
-    Modules.get().add(new SpawnerProtect());
-    Modules.get().add(new AutoSpawnerDeliver());
-    Modules.get().add(new AutoAHSniper());
-    //Modules.get().add(new IDKYET());
-    Modules.get().add(new AntiTrap());
-    Modules.get().add(new AutoShulker());
-	//Modules.get().add(new GrimAutoTotem());//grim auto totem bypass skidded from 
-	Modules.get().add(new HideScoreboard());//yes
-	Modules.get().add(new AdvancedESPPlus());
-	Modules.get().add(new AutoCrystal());//should work
-	Modules.get().add(new RotatedDeepslateESP()); 
+        // Register modules
+        Modules.get().add(new BlockESP());
+        Modules.get().add(new TunnelBaseFinder());
+        Modules.get().add(new SpawnerProtect());
+        Modules.get().add(new AutoSpawnerDeliver());
+        Modules.get().add(new AutoAHSniper());
+        //Modules.get().add(new IDKYET());
+        Modules.get().add(new AntiTrap());
+        Modules.get().add(new AutoShulker());
+        //Modules.get().add(new GrimAutoTotem());
+        Modules.get().add(new HideScoreboard());
+        Modules.get().add(new AdvancedESPPlus());
+        Modules.get().add(new AutoCrystal());
+        Modules.get().add(new RotatedDeepslateESP());
+	    Modules.get().add(new GambleBot());
+
+
+        LOG.info("Soul Addon initialized with {} modules and 3 themes!", Modules.get().getGroup(CATEGORY).size());
     }
 
     @Override
